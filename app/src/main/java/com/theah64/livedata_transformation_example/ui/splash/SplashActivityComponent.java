c0ae3d1e431aebfab283049277f1d8f9a2a57348
theapache64 <theapache64@gmail.com>
2018-12-26 13:30:31 +0530

    Building activity specific components

---
+++ app/src/main/java/com/theah64/livedata_transformation_example/ui/splash/SplashActivityComponent.java
@@ -0,0 +1,4 @@
+package com.theah64.livedata_transformation_example.ui.splash;
+
+public interface SplashActivityComponent {
+}
--- app/src/main/java/com/theah64/livedata_transformation_example/ui/splash/SplashActivityModule.java
+++ app/src/main/java/com/theah64/livedata_transformation_example/ui/splash/SplashActivityModule.java
@@ -1,5 +1,6 @@
 package com.theah64.livedata_transformation_example.ui.splash;
 
+import com.theah64.livedata_transformation_example.data.remote.models.SearchResponse;
 import com.theah64.livedata_transformation_example.di.scopes.PerActivity;
 
 import dagger.Module;
@@ -12,6 +13,11 @@ class SplashActivityModule {
     @Provides
     SplashActivityViewModel provideSplashActivityViewModel() {
         return null;
+    }
+
+    @Provides
+    SearchResponse.User provideUser() {
+        return new SearchResponse.User("Shifar", "dfgsdfg");
     }
 
 }
