65e5e698e47abbe1e035646fc378e9b1976bda4d
theapache64 <theapache64@gmail.com>
2018-12-27 14:43:35 +0530

    Added MainActivtyViewModel to dagger

--- app/src/main/java/com/theah64/livedata_transformation_example/ui/activities/main/MainActivityModule.java
+++ app/src/main/java/com/theah64/livedata_transformation_example/ui/activities/main/MainActivityModule.java
@@ -1,4 +1,4 @@
-package com.theah64.livedata_transformation_example.ui.activities.splash;
+package com.theah64.livedata_transformation_example.ui.activities.main;
 
 import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.ViewModelProviders;
@@ -6,19 +6,18 @@ import dagger.Module;
 import dagger.Provides;
 
 @Module
-public class SplashActivityModule {
+public class MainActivityModule {
 
     private final FragmentActivity fragmentActivity;
 
-    SplashActivityModule(FragmentActivity fragmentActivity) {
+    public MainActivityModule(FragmentActivity fragmentActivity) {
         this.fragmentActivity = fragmentActivity;
     }
 
     @Provides
-    protected SplashActivityViewModel provideViewModel() {
+    MainActivityViewModel provideViewModel() {
         return ViewModelProviders.of(fragmentActivity)
-                .get(SplashActivityViewModel.class);
+                .get(MainActivityViewModel.class);
     }
-
 
 }
